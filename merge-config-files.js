557eda1a31f00ca46f787730e574f74aff569eef
Matthew Lang <matthewlang@hey.com>
2025-01-17 10:26:49 +0000

    Update merge-config-files.js

--- merge-config-files.js
+++ merge-config-files.js
@@ -1,14 +1,24 @@
 const fs = require("fs");
 const _ = require("lodash");
 
-// Load JSON files
-const rootData = JSON.parse(fs.readFileSync("production.json", "utf-8"));
-const overrideData = JSON.parse(fs.readFileSync("preprod-overrides.json", "utf-8"));
+// Generate PreProd config
+const rootConfig = JSON.parse(fs.readFileSync("production.json", "utf-8"));
+const preProdOverrides = JSON.parse(fs.readFileSync("preprod-overrides.json", "utf-8"));
 
 // Deep merge JSON files
-const mergedData = _.merge({}, rootData, overrideData);
+const mergedPreProdConfig = _.merge({}, rootConfig, preProdOverrides);
 
 // Save the merged JSON
-fs.writeFileSync("preprod.json", JSON.stringify(mergedData, null, 2));
+fs.writeFileSync("preprod.json", JSON.stringify(mergedPreProdConfig, null, 2));
+
+// Generate QA config
+const preProdConfig = JSON.parse(fs.readFileSync("preprod.json", "utf-8"));
+const qaOverrides = JSON.parse(fs.readFileSync("qa-overrides.json", "utf-8"));
+
+// Deep merge JSON files
+const mergedQaConfig = _.merge({}, preProdConfig, qaOverrides);
+
+// Save the merged JSON
+fs.writeFileSync("qa.json", JSON.stringify(mergedQaConfig, null, 2));
 
 console.log("JSON files merged successfully!");
